ba2774fb1cb974e752c1d443ad1a5be8285dbe5c
OrlaneMassa <orlanemassa07@gmail.com>
2025-04-08 09:19:44 +0200

    tache2

--- app/src/main/java/com/example/nomdesmembresdugroupe/data/UserCredentials.java
+++ app/src/main/java/com/example/nomdesmembresdugroupe/data/UserCredentials.java
@@ -1,4 +1,22 @@
 package com.example.nomdesmembresdugroupe.data;
+import java.util.HashMap;
 
 public class UserCredentials {
+        private static final HashMap<String, String> USER_CREDENTIALS = new HashMap<>();
+
+        // Méthode statique pour ajouter un nouvel utilisateur
+        public static void addUserCredentials(String username, String password) {
+            // Vérifie si l'utilisateur existe déjà
+            if (!USER_CREDENTIALS.containsKey(username)) {
+                USER_CREDENTIALS.put(username, password);
+                System.out.println("Utilisateur ajouté : " + username);
+            } else {
+                System.out.println("L'utilisateur existe déjà : " + username);
+            }
+        }
+        public static void displayUsers() {
+            for (String username : USER_CREDENTIALS.keySet()) {
+                System.out.println("Utilisateur : " + username);
+            }
+        }
 }
